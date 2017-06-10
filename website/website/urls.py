861be3b996593306e9c1f1e7710e89938b5d6450
cdacosta <c.dacostaf@gmail.com>
2017-06-10 17:09:44 +0200

    upload working

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', include('mainapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+                (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+                    )
