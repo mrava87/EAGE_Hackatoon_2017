d35175bd7ce5048b99b6159f9cf9a98d82da52ae
Filippo B <filippo.broggini@erdw.ethz.ch>
2017-06-11 12:15:09 +0200

    Less traps

--- examples/Create_Synthetic_Model.py
+++ examples/Create_Synthetic_Model.py
@@ -64,15 +64,26 @@ Trap0.Apply()
 plt.imshow(Trap0.V)
 plt.colorbar()
 
-# %% Combine models
+# %% synthetic_model
 
-Fake = np.concatenate((Layers0.V, Fault0.V, Layers0.V), axis=0)
-Fake[270:370, 600:700] = Fake[270:370, 600:700] + Trap0.V
-Fake[400:500, 650:750] = Fake[400:500, 650:750] + Trap0.V
-Fake[100:200, 650:750] = Fake[100:200, 650:750] + Trap0.V
-Fake[200:300, 150:250] = Fake[200:300, 150:250] + Trap0.V
-Fake[350:450, 250:350] = Fake[350:450, 250:350] + Trap0.V
-Fake[300:400, 750:850] = Fake[300:400, 750:850] + Trap0.V
+model = 1
+
+if model==0:
+    Fake = np.concatenate((Layers0.V, Fault0.V, Layers0.V), axis=0)
+    Fake[270:370, 600:700] = Fake[270:370, 600:700] + Trap0.V
+    Fake[400:500, 650:750] = Fake[400:500, 650:750] + Trap0.V
+    Fake[100:200, 650:750] = Fake[100:200, 650:750] + Trap0.V
+    Fake[200:300, 150:250] = Fake[200:300, 150:250] + Trap0.V
+    Fake[350:450, 250:350] = Fake[350:450, 250:350] + Trap0.V
+    Fake[300:400, 750:850] = Fake[300:400, 750:850] + Trap0.V
+if model==1:
+    Fake = np.concatenate((Layers0.V, Fault0.V, Layers0.V), axis=0)
+#    Fake[270:370, 600:700] = Fake[270:370, 600:700] + Trap0.V
+    Fake[400:500, 650:750] = Fake[400:500, 650:750] + Trap0.V
+    Fake[100:200, 650:750] = Fake[100:200, 650:750] + Trap0.V
+    Fake[200:300, 150:250] = Fake[200:300, 150:250] + Trap0.V
+#    Fake[350:450, 250:350] = Fake[350:450, 250:350] + Trap0.V
+#    Fake[300:400, 750:850] = Fake[300:400, 750:850] + Trap0.V
 #Fake[300:400, 600:700] = Trap0.V
 
 plt.figure(figsize=(12, 7))
@@ -92,7 +103,7 @@ Seismod = SM.SeismicModelling2D({'V'      : Fake,
 Seismod.Apply()
 Seismod.Visualize(cbarlims=[-4e12,4e12])
 
-sp.imsave('../datasets/seismic/synthetics/targets/synthetic_model.png',Seismod.stack)
+sp.imsave('../datasets/seismic/synthetics/targets/synthetic_model' + str(model) + '.png',Seismod.stack)
 
 # %%
 #filepath='/home/bfilippo/Work/EAGE_Hackatoon_2017/datasets/faults/'
